328c96fd5a07cd2d2222ffbe70e8205ef7e487b3
samufigh <samufigh21@gmail.com>
2023-05-31 15:18:37 -0300

    rotas adicionadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Cadastro from './pages/Cadastro/Cadastro';
+import Habitos from './pages/Habitos/Habitos';
+import Hoje from "./pages/Hoje/Hoje";
+import Historico from './pages/Historico/Historico';
 
 function App() {
 
   return (
     <>
-      oi
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path="/cadastro" element={<Cadastro />}/>
+          <Route path="/habitos" element={<Habitos />}/>
+          <Route path="/hoje" element={<Hoje />}/>
+          <Route path="/historico" element={<Historico />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Habitos/Habitos.jsx
@@ -0,0 +1,7 @@
+export default function Habitos(){
+    return(
+        <>
+            Habitos
+        </>
+    )
+}
